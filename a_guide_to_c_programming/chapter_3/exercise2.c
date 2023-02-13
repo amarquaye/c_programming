f2bdcedd7bca763f60d9f428098f6625dc062475
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-02-13 22:02:49 +0000

    Initial commit

---
+++ a_guide_to_c_programming/chapter_3/exercise2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* C program to calculate and display the sum of the integers from 1 to 5 */
+
+main(){
+    int num1 = 1;
+    int num2 = 2;
+    int num3 = 3;
+    int num4 = 4;
+    int num5 = 5;
+
+    int sum;
+
+    sum = num1 + num2 + num3 + num4 + num5;
+
+    printf("The sum is %d", sum);
+
+}
